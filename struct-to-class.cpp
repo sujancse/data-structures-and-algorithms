3a2f0518340a19117da86a42276bb3306a0338b9
Sujan Mahmud <sujancseruet@gmail.com>
2020-11-11 07:48:49 +0600

    c structures to cpp class

---
+++ struct-to-class.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+class Rectangle
+{
+    private:
+        int length;
+        int breadth;
+
+    public:
+        Rectangle(int l, int b)
+        {
+            length = l;
+            breadth = b;
+        }
+
+        int area()
+        {
+            return length * breadth;
+        }
+
+        void changeLength(int l)
+        {
+            length = l;
+        }
+};
+
+int main()
+{
+    Rectangle r(10, 5);
+    cout<<r.area();
+    r.changeLength(20);
+    cout<<r.area();
+}
--- structure-as-parameter.c
+++ structure-as-parameter.c
@@ -6,6 +6,12 @@ struct Rectangle
     int breadth;
 };
 
+struct Test
+{
+    int A[5];
+    int n;
+}
+
 void changeLength(struct Rectangle *p, int l)
 {
     return p->length = l;
@@ -21,4 +27,6 @@ int main()
     struct Rectangle r = {10, 5};
     changeLength(&r, 20);
     printf("%d", area(&r));
+
+    struct Test t = {{2, 4, 6, 8, 10}, 5};
 }
