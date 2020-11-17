eb714b509e6dbea0ac79e866b138c22e42bac15c
Sujan Mahmud <sujancseruet@gmail.com>
2020-11-17 21:11:12 +0600

    c pointer to structures

--- practice-pointer-to-structure.cpp
+++ practice-pointer-to-structure.cpp
@@ -21,24 +21,24 @@ int main()
 //    cout << p->breadth << endl;
 
     // In c language
-//    struct Rectangle *p;
-//    p = (struct Rectangle *)malloc(sizeof(struct Rectangle));
-//
-//    p->length = 20;
-//    p->breadth = 10;
-//
-//    cout << p->length << endl;
-//    cout << p->breadth << endl;
+    struct Rectangle *p;
+    p = (struct Rectangle *)malloc(sizeof(struct Rectangle));
 
-    // In cpp
-    Rectangle *p;
-    p = new Rectangle;
-
-    p->length = 30;
-    p->breadth = 20;
+    p->length = 20;
+    p->breadth = 10;
 
     cout << p->length << endl;
     cout << p->breadth << endl;
+
+    // In cpp
+//    Rectangle *p;
+//    p = new Rectangle;
+//
+//    p->length = 30;
+//    p->breadth = 20;
+//
+//    cout << p->length << endl;
+//    cout << p->breadth << endl;
 
     return 0;
 }
