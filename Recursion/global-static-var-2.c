0e0705fda5607b5f390b54904bffafa6195389c5
Sujan Mahmud <sujancseruet@gmail.com>
2020-12-09 08:23:34 +0600

    static and global var in recursion

---
+++ Recursion/global-static-var-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int x = 0;
+int fun(int n)
+{
+//    int static x = 0;
+
+    if(n>0) {
+        x++;
+        return fun(n-1) + x;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int a = 5;
+    printf("%d", fun(a));
+
+    return 0;
+}
