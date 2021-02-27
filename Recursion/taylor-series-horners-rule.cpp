a5b74c14fbe5192626da23b186f70734e5f3cde8
Sujan Mahmud <sujancseruet@gmail.com>
2021-02-27 21:46:36 +0600

    taylor series horners rule

---
+++ Recursion/taylor-series-horners-rule.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+// using loop
+double e(int x, int n)
+{
+    double s = 1;
+    double num = 1;
+    double den = 1;
+
+    for(int i = 1; i <= n; i++) {
+        num*= x;
+        den*= i;
+
+        s+= num/den;
+    }
+
+    return s;
+}
+
+double e1(int x, int n)
+{
+    static double s = 1;
+
+    if (n == 0)
+        return s;
+
+    s = 1 + x*s/n;
+
+    return e1(x, n-1);
+}
+
+int main()
+{
+    printf("%lf \n", e(1, 10));
+    printf("%lf", e1(1, 10));
+}
