b0964fb31d8adc196450f83797e552d567192cb5
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-02 12:03:32 +0600

    quiz 4

---
+++ Recursion/quiz/quiz-4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int fun(int n)
+{
+    int x = 1, k;
+
+    if (n==1) return x;
+
+    for (k=1; k < n; ++k) {
+        x = x + fun(k) * fun(n-k);
+    }
+
+    return x;
+}
+
+int main()
+{
+    printf("%d", fun(3));
+}
