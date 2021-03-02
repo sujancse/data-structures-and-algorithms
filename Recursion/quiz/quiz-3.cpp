f1332c65fde72900fb27f4990aa36b1817044275
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-02 12:03:21 +0600

    quiz 3

---
+++ Recursion/quiz/quiz-3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int f(int &x, int c)
+{
+    c = c - 1;
+
+    if(c==0) return 1;
+
+    x = x + 1;
+
+    return f(x, c)*x;
+}
+
+int main()
+{
+    int p = 5;
+
+    printf("%d", f(p, p));
+}
