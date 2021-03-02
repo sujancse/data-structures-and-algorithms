394557856dc0d278745427f5ece6f21d96c90445
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-02 12:03:44 +0600

    quiz 5

---
+++ Recursion/quiz/quiz-5.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void count(int n)
+{
+    static int d = 1;
+
+    printf("%d", n);
+    printf("%d", d);
+
+    d++;
+
+    if (n>1) count(n-1);
+
+    printf("%d", d);
+}
+
+int main()
+{
+    count(3);
+
+    return 0;
+}
