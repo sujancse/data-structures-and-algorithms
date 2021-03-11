a7d271319cad06cd46518fcb11a7f15683d8e4e5
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-11 09:33:08 +0600

    array append

--- Arrays/ADT/demo.c
+++ Arrays/ADT/demo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 struct Array {
-    int A[30];
+    int A[10];
     int size;
     int length;
 };
@@ -10,13 +10,21 @@ void Display(struct Array arr)
 {
     int i;
 
-    for(i = 0; i < 4; i++)
+    for(i = 0; i < arr.length; i++)
         printf("%d \n", arr.A[i]);
+}
+
+void Append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+        arr->A[arr->length++] = x;
 }
 
 int main()
 {
-    struct Array arr = {{1, 2, 3, 4}, 30, 4};
+    struct Array arr = {{1, 2, 3, 4}, 10, 4};
+
+    Append(&arr, 10);
 
     Display(arr);
 
