082c8cbf02bc2ef0862bb69dcc047637d7ff5b9e
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-15 13:26:04 +0600

    binary search

--- Arrays/ADT/demo.c
+++ Arrays/ADT/demo.c
@@ -38,6 +38,41 @@ void Delete(struct Array *arr, int index)
         arr->length--;
 }
 
+int LinearSearch(struct Array *arr, int x)
+{
+    int i;
+
+    for (i = 0; i < arr->length; i++) {
+        if (arr->A[i] == x) {
+            //swap(&arr->A[i], &arr->A[i-1]);
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int BinarySearch(struct Array arr, int key)
+{
+    int mid;
+    int l = 0;
+    int h = arr.length - 1;
+
+    while (h >= l) {
+        mid = (l+h)/2;
+
+        if (key == arr.A[mid]) {
+            return mid;
+        } else if (key < arr.A[mid]) {
+            h = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{1, 2, 3, 4}, 10, 4};
@@ -46,9 +81,13 @@ int main()
 
     //Append(&arr, 10);
 
-    Delete(&arr, 0);
+    //Delete(&arr, 0);
 
-    Display(arr);
+    //printf("%d \n", LinearSearch(&arr, 3));
+
+    printf("%d", BinarySearch(arr, 4));
+
+    //Display(arr);
 
     return 0;
 }
