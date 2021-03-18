c72e7e233a520f27c995c29c419a2ebc62790310
Sujan Mahmud <sujancseruet@gmail.com>
2021-03-18 12:41:31 +0600

    get, set, sum, min, max, avg

--- Arrays/ADT/demo.c
+++ Arrays/ADT/demo.c
@@ -73,9 +73,81 @@ int BinarySearch(struct Array arr, int key)
     return -1;
 }
 
+int Get(struct Array arr, int index) {
+    if (index >= 0 && index <= arr.length)
+        return arr.A[index];
+
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if (index >=0 && index <=  arr->length) {
+        arr->A[index] = x;
+    }
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+
+    for (int i = 0; i < arr.length; i++) {
+        if (max < arr.A[i])
+            max = arr.A[i];
+    }
+
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+
+    for (int i = 0; i < arr.length; i++) {
+        if (min > arr.A[i])
+            min = arr.A[i];
+    }
+
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+        sum += arr.A[i];
+    }
+
+    return sum;
+}
+
+int RSum(struct Array arr, int n)
+{
+    if (n == 0)
+        return arr.A[0];
+    return RSum(arr, n-1) + arr.A[n];
+}
+
+int Avg(struct Array arr)
+{
+    return (float) Sum(arr)/arr.length;
+}
+
 int main()
 {
-    struct Array arr = {{1, 2, 3, 4}, 10, 4};
+    struct Array arr = {{2, 3, 9, 1}, 10, 4};
+
+    printf("%d \n", Avg(arr));
+
+    //printf("%d \n", RSum(arr, 3));
+
+    //printf("%d \n", Sum(arr));
+
+    //printf("%d \n", Min(arr));
+
+    //printf("%d \n", Max(arr));
+
+    //printf("%d \n", Get(arr, 2));
 
     //Insert(&arr, 2, 5);
 
@@ -85,7 +157,7 @@ int main()
 
     //printf("%d \n", LinearSearch(&arr, 3));
 
-    printf("%d", BinarySearch(arr, 4));
+    //printf("%d", BinarySearch(arr, 4));
 
     //Display(arr);
 
