761b488d2afd7f0fa95d7fc2de966e586e7503a2
Sujan Mahmud <sujancseruet@gmail.com>
2021-06-03 08:17:10 +0600

    set operations

--- Arrays/ADT/demo.c
+++ Arrays/ADT/demo.c
@@ -266,14 +266,110 @@ struct Array * Merge2(struct Array arr1, struct Array arr2)
     return arr3;
 }
 
+struct Array * Union(struct Array arr1, struct Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1.length && j < arr2.length)
+    {
+        if (arr1.A[i] < arr2.A[j]) {
+            arr3->A[k++] = arr1.A[i++];
+        } else if (arr1.A[i] > arr2.A[j]) {
+            arr3->A[k++] = arr2.A[j++];
+        } else if (arr1.A[i] == arr2.A[j]) {
+            arr3->A[k++] = arr1.A[i++];
+            j++;
+        }
+    }
+
+    // for the rest of arr1
+    for(; i<arr1.length; i++) {
+        arr3->A[k++] = arr1.A[i];
+    }
+
+    // for the rest of arr2
+    for(; j<arr2.length; j++) {
+        arr3->A[k++] = arr2.A[j];
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array * Intersection(struct Array arr1, struct Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1.length && j < arr2.length)
+    {
+        if (arr1.A[i] < arr2.A[j]) {
+            i++;
+        } else if (arr1.A[i] > arr2.A[j]) {
+            j++;
+        } else if (arr1.A[i] == arr2.A[j]) {
+            arr3->A[k++] = arr1.A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+struct Array * Difference(struct Array arr1, struct Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array*)malloc(sizeof(struct Array));
+
+    while (i < arr1.length && j < arr2.length)
+    {
+        if (arr1.A[i] < arr2.A[j]) {
+            arr3->A[k++] = arr1.A[i++];
+        } else if (arr1.A[i] > arr2.A[j]) {
+            j++;
+        } else if (arr1.A[i] == arr2.A[j]) {
+            i++;
+            j++;
+        }
+    }
+
+    // for the rest of arr1
+    for(; i<arr1.length; i++) {
+        arr3->A[k++] = arr1.A[i];
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
     struct Array arr = {{2, -3, 9, -10}, 10, 4};
 
-    struct Array arr1 = {{2, 4, 6, 8}, 10, 4};
-    struct Array arr2 = {{1, 3, 5, 7}, 10, 4};
+    struct Array arr1 = {{2, 3, 6, 8}, 10, 4};
+    struct Array arr2 = {{1, 3, 5, 6}, 10, 4};
 
     struct Array *arr3;
+
+    //arr3 = Union(arr1, arr2);
+    //arr3 = Intersection(arr1, arr2);
+    arr3 = Difference(arr1, arr2);
+
+    Display(*arr3);
 
     //arr3 = Merge(arr1, arr2);
 
@@ -311,9 +407,7 @@ int main()
 
     //printf("%d", BinarySearch(arr, 4));
 
-    arr3 = Merge2(arr1, arr2);
-
-    Display(*arr3);
+    //arr3 = Merge2(arr1, arr2);
 
     //Display(arr);
 
